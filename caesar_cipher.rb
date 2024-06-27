942ec7b5ace592f56a88939986b53b5cea3995fa
SpeakBoy <speakman@mcmaster.ca>
2024-06-27 03:01:36 -0400

    Correct Rubocop offenses

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -4,30 +4,26 @@ ASCII_DOWNCASE_MIN = 97
 ASCII_DOWNCASE_MAX = 122
 
 def caesar_cipher(input_string, shift_val)
-    char_array = input_string.split("")
-    new_string = ""
-    char_array.each do |char|
-        ascii_val = char.ord
-        shifted_ascii_val = shift_ascii_val(ascii_val, shift_val)
-        shifted_ascii_char = shifted_ascii_val.chr
-        new_string += shifted_ascii_char
-    end
-    new_string
+  char_array = input_string.split('')
+  new_string = ''
+  char_array.each do |char|
+    ascii_val = char.ord
+    shifted_ascii_val = shift_ascii_val(ascii_val, shift_val)
+    shifted_ascii_char = shifted_ascii_val.chr
+    new_string += shifted_ascii_char
+  end
+  new_string
 end
 
 def shift_ascii_val(ascii_val, shift_val)
-    if ascii_val.between?(ASCII_UPCASE_MIN, ASCII_UPCASE_MAX)
-        while ascii_val + shift_val > ASCII_UPCASE_MAX
-            ascii_val -= 26
-        end
-        ascii_val += shift_val
-    elsif ascii_val.between?(ASCII_DOWNCASE_MIN, ASCII_DOWNCASE_MAX)
-        while ascii_val + shift_val > ASCII_DOWNCASE_MAX
-            ascii_val -= 26
-        end
-        ascii_val += shift_val
-    end
-    ascii_val
+  if ascii_val.between?(ASCII_UPCASE_MIN, ASCII_UPCASE_MAX)
+    ascii_val -= 26 while ascii_val + shift_val > ASCII_UPCASE_MAX
+    ascii_val += shift_val
+  elsif ascii_val.between?(ASCII_DOWNCASE_MIN, ASCII_DOWNCASE_MAX)
+    ascii_val -= 26 while ascii_val + shift_val > ASCII_DOWNCASE_MAX
+    ascii_val += shift_val
+  end
+  ascii_val
 end
 
-puts caesar_cipher("My name is Nevin", 300)
+puts caesar_cipher('My name is Nevin', 300)
